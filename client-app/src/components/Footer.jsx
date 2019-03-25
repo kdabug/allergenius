797454689c04956a8222146949602b66e63c9d5f
Mikayda Mills <mills.ma12@gmail.com>
2019-03-24 20:33:55 -0400

    added Header and Footer stufffff

---
+++ client-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = props => {
+  return (
+    <div className="footer">
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/privacy">Privacy</Link>
+        <Link to="/FAQ">FAQ</Link>
+        <Link to="/contact">Contact Us</Link>
+      </nav>
+    </div>
+  );
+};
+export default withRouter(Footer);

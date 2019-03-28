3fcb211991609093400e4d7a973d8ea2e6a29691
aefritz <austin.fritz33@gmail.com>
2019-03-27 21:29:38 -0400

    added blogposts route and allergies route

--- routes/allergiesRouter.js
+++ routes/allergiesRouter.js
@@ -1,5 +1,55 @@
 const { Router } = require('express');
+const { User, UserAllergy, Allergy } = require('../models');
 
 const allergiesRouter = Router();
+
+allergiesRouter.use((req, res, next) => {
+  console.log("allergy router triggered");
+  next();
+});
+
+//get list of hardcoded allergies
+allergiesRouter.get('/', async (req,res) => {
+  try {
+    console.log('main route triggered');
+    const resp = await Allergy.findAll();
+    res.json(resp);
+  } catch(e) {
+    res.status(403).message('invalid request')
+  }
+});
+
+
+//set an allergies for a user, we could make this an array
+allergiesRouter.post('/:user_id', async (req,res) => {
+  try {
+    const {user_id} = req.params;
+    const {allergy_id} = req.body;
+    const resp = await UserAllergy.create({
+      userId: user_id,
+      allergyId: allergy_id
+    });
+    res.json(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(403);
+  }
+});
+
+//get allergies for a user
+allergiesRouter.get('/:id', async (req,res) => {
+  try {
+    const {id} = req.params;
+    const user = await User.findOne({
+      where: {id}
+    });
+    const resp = await user.getAllergies().then(resp => resp);
+    res.json(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(403).message('invalid request');
+  }
+});
+
 
 module.exports = allergiesRouter;
--- routes/blogpostsRouter.js
+++ routes/blogpostsRouter.js
@@ -1,5 +1,67 @@
 const { Router } = require('express');
-
+const {User, Blogpost, Country} = require('../models');
 const blogpostsRouter = Router();
+
+blogpostsRouter.use((req, res, next) => {
+  console.log("blogpost route triggered")
+  next();
+})
+
+
+//get all blogposts
+blogpostsRouter.get('/', async (req, res) => {
+  try {
+    const resp = await Blogpost.findAll();
+    res.json(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(403);
+  }
+});
+
+
+//make blogpost ... user_id is the id of the user making the post
+blogpostsRouter.post('/user/:user_id', async (req, res) => {
+  try {
+    const {user_id} = req.params;
+    const {countryId, title, content} = req.body;
+    const resp = await Blogpost.create({
+      countryId,
+      title,
+      content,
+      userId: user_id});
+    res.json(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(403);
+  }
+})
+
+//get a user's blogposts
+blogpostsRouter.get('/user/:user_id', async (req, res) => {
+  try {
+    const {user_id} = req.params;
+    const user = await User.findOne({where: {id: user_id}});
+    const resp = await user.getBlogposts().then(resp => resp);
+    res.json(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(403);
+  }
+})
+
+//get a country's blogposts
+blogpostsRouter.get('/country/:country_id', async (req, res) => {
+  try {
+    const {country_id} = req.params;
+    const country = await Country.findOne({where: {id: country_id}});
+    const resp = await country.getBlogposts().then(resp => resp);
+    res.json(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(403);
+  }
+})
+
 
 module.exports = blogpostsRouter;

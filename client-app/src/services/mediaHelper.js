16c88ec1cf3cb3c864f4190499019d8844f5de86
aefritz <austin.fritz33@gmail.com>
2019-03-30 13:38:02 -0400

    removed api key oops

--- client-app/src/services/mediaHelper.js
+++ client-app/src/services/mediaHelper.js
@@ -1,10 +1,11 @@
+let api_key = process.env.REACT_APP_PLACES_API_KEY;
 let axios = require('axios');
 async function getMedia (city_string) {
   try {
     city_string.replace(/ /g, '%20');
-    let location_match = await axios(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city_string}&inputtype=textquery&fields=photos&key=AIzaSyBskCvItiXY5uZwJC2UwfkI6dQWOyoDiCk`);
+    let location_match = await axios(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city_string}&inputtype=textquery&fields=photos&key=${api_key}`);
     let photoreference = location_match.data.candidates[0].photos[0].photo_reference;
-    let photo_resp = await axios(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoreference}&key=AIzaSyBskCvItiXY5uZwJC2UwfkI6dQWOyoDiCk`);
+    let photo_resp = await axios(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoreference}&key=${api_key}`);
     return photo_resp.request.socket._httpMessage._redirectable._currentUrl;
   } catch(e) {
     console.error(e);

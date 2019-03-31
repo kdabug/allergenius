0f7a6a4386fd440f5914a93be1e092913795abbd
Daniel Gillen <danielgillen@Daniels-MBP.home>
2019-03-31 15:59:51 -0400

    styled home page, fixed placement of query bar

--- client-app/src/components/ExploreHome.jsx
+++ client-app/src/components/ExploreHome.jsx
@@ -35,40 +35,42 @@ class ExploreHome extends React.Component {
       <>
         {!this.state.fetchingMedia && (
           <div className="explore-home-container">
-            <h1>Explore Home</h1>
+            <h1>Explore Allergenius</h1>
             <div className="preset-cities-container">
+            <div className="circle"></div>
               <h2>Places</h2>
-              <div>
+              <div className="city-container">
                 <Icon url={this.state.mediaArray[0]} />
                 <Link to={"/places-cities/London/"}>London, England</Link>
               </div>
-              <div>
+              <div className="city-container">
                 <Icon url={this.state.mediaArray[1]} />
                 <Link to={"/places-cities/Paris/2974"}>Paris, France</Link>
               </div>
-              <div>
+              <div className="city-container">
                 <Icon url={this.state.mediaArray[2]} />
                 <Link to={"/places-cities/Tokyo/1532"}>Tokyo, Japan</Link>
               </div>
-              <div>
+              {/*<div>
                 <Icon url={this.state.mediaArray[3]} />
                 <Link to={"/places-cities/Rio De Janiero/"}>
                   Rio De Janiero, Brazil
                 </Link>
-              </div>
+              </div>*/}
             </div>
-            <div className="preset-allergens-container">
+            <div className="preset-cities-container">
+              <div className="circle"></div>
               <h2>Allergens</h2>
-              <div>
+              <div className="city-container">
                 icon & <Link to={"/food-allergens/"}>Shellfish</Link>
               </div>
-              <div>
+              <div className="city-container">
                 icon & <Link to={"/food-allergens/"}>Peanuts</Link>
               </div>
-              <div>
+              <div className="city-container">
                 icon & <Link to={"/food-allergens/"}>Eggs</Link>
               </div>
-              <div>
+              <div className="city-container">
                 icon & <Link to={"/food-allergens/"}>Tree Nut</Link>
               </div>
             </div>
